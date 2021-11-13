a601c768816cd551a43edd3dec47d3f3e2a3cb4a
Daisyzhao21 <91299764+Daisyzhao21@users.noreply.github.com>
2021-11-13 13:04:09 -0800

    Update Employee_Database_challenge.sql

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -11,7 +11,6 @@ INNER JOIN titles as t
 ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 order by e.emp_no;
-
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no,
 first_name,
@@ -20,7 +19,6 @@ title
 INTO unique_titles
 FROM retirement_titles
 ORDER BY emp_no, title DESC;
-
 -- Retrieve the number of employees by their most recent job title who are about to retire.
 SELECT COUNT(ut.emp_no),
 ut.title
@@ -28,7 +26,6 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY title 
 ORDER BY COUNT(title) DESC;
-
 
 -- DELIVERABLE 2: The Employees Eligible for the Mentorship Program
 -- Write a query to create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program.
